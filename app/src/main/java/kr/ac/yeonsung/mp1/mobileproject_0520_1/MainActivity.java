aa0bf95c80307d4dd7248381644006d2ebcafc15
HSsangmin <tkdals4104@naver.com>
2022-05-20 10:04:40 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/yeonsung/mp1/mobileproject_0520_1/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.yeonsung.mp1.mobileproject_0520_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_date_picker);
+    }
+}
